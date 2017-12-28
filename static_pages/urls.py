7cb3114235b12ca5e11702093e8edd90c5c65b01
Patricia Penton <ppenton@23andme.com>
2017-12-28 10:53:41 -0800

    Mobile and tablet responsive clean up

--- static_pages/urls.py
+++ static_pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
+    url(r'resume/', views.resume, name='resume'),
 ]
--- static_pages/views.py
+++ static_pages/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'static_pages/homepage.html')
+
+
+def resume(request):
+    return render(request, 'static_pages/resume.html')
