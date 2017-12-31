e9ab107efe156735b2369a44ada036aea0a68975
Patricia Penton <ppenton@23andme.com>
2017-12-31 11:22:37 -0800

    Fix errors in wsgi.py

--- main_site/wsgi.py
+++ main_site/wsgi.py
@@ -15,7 +15,8 @@ from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main_site.settings")
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root='/static/')
-application.add_files('/static/')
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, '/static/'))
