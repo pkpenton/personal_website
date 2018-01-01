d9f9ca04c454a280af15f409c774637ade77a922
Patricia Penton <ppenton@23andme.com>
2018-01-01 12:48:29 -0800

    Add favicon, fix hamburger dropdown on mobile nav

--- static_pages/urls.py
+++ static_pages/urls.py
@@ -1,7 +1,18 @@
 from django.conf.urls import url
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 from . import views
+
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
     url(r'resume/', views.resume, name='resume'),
+    url(
+        r'^favicon.ico$',
+        RedirectView.as_view(
+            url=staticfiles_storage.url('favicon.ico'),
+            permanent=False),
+        name="favicon"
+    ),
 ]
