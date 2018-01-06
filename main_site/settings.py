fd9edabf1c18b88e904a5b200452b27aace36607
Patricia Penton <ppenton@23andme.com>
2018-01-05 21:02:28 -0800

    Add dj_database_url to pipenv and settings

--- main_site/settings.py
+++ main_site/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-from whitenoise import WhiteNoise
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,6 +87,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
