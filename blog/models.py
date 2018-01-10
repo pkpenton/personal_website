fb008d782926d40a74866732fffde7852d855dd0
Patricia Penton <ppenton@23andme.com>
2018-01-10 08:33:45 -0800

    Remove wysiwyg editor, just use html

--- blog/models.py
+++ blog/models.py
@@ -3,17 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
-from ckeditor_uploader.fields import RichTextUploadingField
-
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
-    text = RichTextUploadingField()
+    text = models.TextField()
     text_preview = models.TextField(blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    list_image = models.ImageField(upload_to='images/', blank=True, null=True)
+    image_1 = models.ImageField(upload_to='images/', blank=True, null=True)
+    image_2 = models.ImageField(upload_to='images/', blank=True, null=True)
+    image_3 = models.ImageField(upload_to='images/', blank=True, null=True)
     medium_post = models.BooleanField(default=False)
     medium_url = models.URLField(blank=True, null=True)
 
